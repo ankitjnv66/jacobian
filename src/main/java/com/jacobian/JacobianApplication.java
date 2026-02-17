0ec275d90853bdc64153960f560adae2f2404f33
ankit singh <ankitsingh@MacBookAir.lan>
2026-02-17 23:25:39 +0530

    initial setup

---
+++ src/main/java/com/jacobian/JacobianApplication.java
@@ -0,0 +1,13 @@
+package com.jacobian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacobianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacobianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jacobian/JacobianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacobian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacobianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
